8d0900e70d88afbd9f05d367b395184baec78d8b
AnkitGurung <ankitgurung234@gmail.com>
2025-04-19 10:51:13 +0545

    filtering on borrowed books

--- src/components/table/LibrarianAndAdmin/catalog/BorrowedBook/data-table.jsx
+++ src/components/table/LibrarianAndAdmin/catalog/BorrowedBook/data-table.jsx
@@ -170,8 +170,8 @@ export function DataTable({ columns, data }) {
   });
 
   return (
-    <div>
-      <div className="flex items-center py-1 justify-between pb-4">
+    <>
+      <div className="flex items-center py-1 justify-between pb-4 bg-gray-100">
         <Input
           className="w-1/4 h-10 bg-white"
           placeholder="Search by name..."
@@ -182,7 +182,7 @@ export function DataTable({ columns, data }) {
         />
         <div className="flex gap-2.5">
           <select
-            className="border rounded px-3 py-2"
+            className="border rounded-lg text-gray-800 px-3 py-2 bg-white "
             value={table.getColumn("extended")?.getFilterValue() ?? ""}
             onChange={(e) =>
               table.getColumn("extended")?.setFilterValue(e.target.value)
@@ -195,11 +195,9 @@ export function DataTable({ columns, data }) {
             <option value={true}>YES</option>
             <option value={false}>NO</option>
           </select>
-        </div>
 
-        <div className="flex gap-2.5">
           <select
-            className="border rounded px-3 py-2"
+            className="border rounded-lg px-3 py-2  bg-white"
             value={table.getColumn("returnStatus")?.getFilterValue() ?? ""}
             onChange={(e) =>
               table.getColumn("returnStatus")?.setFilterValue(e.target.value)
@@ -215,80 +213,80 @@ export function DataTable({ columns, data }) {
         </div>
       </div>
 
-      <div className="flex items-center py-4 justify-between"></div>
-
-      <div className="rounded-md">
-        <Table>
-          <TableHeader className="border-b-[2px] border-[rgba(0,0,0,0.5)]">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead
-                      key={header.id}
-                      className="text-gray-700 font-medium uppercase"
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
+      <div className="bg-gray-100">
+        <div className="rounded-md">
+          <Table>
+            <TableHeader className="border-b-[2px] border-[rgba(0,0,0,0.5)]">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead
+                        key={header.id}
+                        className="text-gray-700 font-medium uppercase"
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
 
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
