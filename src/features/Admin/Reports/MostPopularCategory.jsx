b010b7dc4695deb5bf2f505d31f64eb930b0023c
AnkitGurung <ankitgurung234@gmail.com>
2025-04-19 10:48:13 +0545

    most popular category report

--- src/features/Admin/Reports/MostPopularCategory.jsx
+++ src/features/Admin/Reports/MostPopularCategory.jsx
@@ -1,5 +1,28 @@
+import { columns } from "@/components/table/Admin/reports/MostPopularCategory/columns";
+import { DataTable } from "@/components/table/Admin/reports/MostPopularCategory/data-table";
+import { useFetchMostPopularCategory } from "@/hooks/Report/useFetchMostPopularCategory";
+import GLOBAL_SERVICE from "@/services/GlobalServices";
+import { useEffect } from "react";
+
 export const MostPopularCategory = () => {
+  const { data: mostPopularCategory, refetch: refetchMostPopularCategory } =
+    useFetchMostPopularCategory();
+
+  useEffect(() => {
+    refetchMostPopularCategory();
+  }, []);
+
+  useEffect(() => {
+    console.log(mostPopularCategory);
+  }, [mostPopularCategory]);
+
   return (
-    <div>MostPopularCategory</div>
-  )
-}
+    <section className="bg-gray-100 min-h-screen">
+      {mostPopularCategory?.data && (
+        <div>
+          <DataTable columns={columns} data={mostPopularCategory?.data} />
+        </div>
+      )}
+    </section>
+  );
+};
