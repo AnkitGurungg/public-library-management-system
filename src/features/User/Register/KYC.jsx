87d577857e6aa5bde8c76cf9e5e051f27fe42fa8
AnkitGurung <ankitgurung234@gmail.com>
2025-04-22 10:31:56 +0545

    kyc form

--- src/features/User/Register/KYC.jsx
+++ src/features/User/Register/KYC.jsx
@@ -17,6 +17,8 @@ import { useFetchNonVerifiedMembers } from "@/hooks/useFetchNonVerifiedMembers";
 import GLOBAL_SERVICE from "@/services/GlobalServices";
 import { useFetchUserProfile } from "@/hooks/useFetchUserProfile";
 import toast from "react-hot-toast";
+import { Textarea } from "@/components/ui/textarea";
+import { IdCard } from "lucide-react";
 
 const KYC = () => {
   const {
@@ -94,54 +96,87 @@ const KYC = () => {
         toast.error("Internal Server Error!");
       }
     }
+
+    const resetForm = () => {
+      reset();
+      setUserImage(null);
+      setEvidenceImages([null, null]);
+      setError(null);
+
+      if (e1Ref.current) e1Ref.current.value = "";
+      if (e2Ref.current) e2Ref.current.value = "";
+    };
   };
 
   return (
     <div>
       <Dialog>
         <DialogTrigger asChild>
-          <button className="text-[18px]">KYC</button>
+          <Button variant="ghost" className="text-lg font-medium">
+            KYC
+          </Button>
         </DialogTrigger>
-        <DialogContent className="w-390" aria-describedby={undefined}>
+        <DialogContent className="sm:max-w-[500px]">
           <DialogHeader>
-            <DialogTitle>KYC Verification</DialogTitle>
+            <DialogTitle className="text-2xl font-semibold  flex items-center mb-0">
+              <div className="h-12 bg-[#d7d7d7] w-12 flex justify-center items-center rounded-md mr-3">
+                <IdCard size={35} />
+              </div>
+              KYC Verification
+            </DialogTitle>
           </DialogHeader>
-          <hr />
-          <ScrollArea className="h-[70vh]">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div>
-                <div>
-                  <Label htmlFor="name">Name</Label>
+          <div className="my-0 h-px w-full bg-gray-800" />
+          <ScrollArea className="max-h-[70vh] px-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+              <div className="space-y-4">
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="name"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Name
+                  </Label>
                   <Input
                     id="name"
                     type="text"
-                    defaultValue=""
+                    placeholder="Enter full name"
+                    className="h-12 border border-gray-300 mb-0"
                     {...register("name", {
                       required: "Please enter name!",
                       minLength: {
                         value: 5,
-                        message: "Min lenght should be 5",
+                        message: "Min length should be 5",
                       },
                       maxLength: {
                         value: 20,
-                        message: "Max lenght should be 20",
+                        message: "Max length should be 20",
                       },
                     })}
                   />
-                  <p>{errors?.name?.message}</p>
+                  {errors?.name?.message && (
+                    <p className="text-sm text-red-500 mt-0.5">
+                      {errors.name.message}
+                    </p>
+                  )}
                 </div>
 
-                <div>
-                  <Label htmlFor="title">Contact</Label>
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="contact"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Contact
+                  </Label>
                   <Input
                     id="contact"
                     type="text"
-                    defaultValue=""
+                    placeholder="Enter contact number"
+                    className="h-12 border border-gray-300 mb-0"
                     {...register("contactNumber", {
                       required: "Please enter contact!",
                       minLength: {
                         value: 10,
-                        message: "Please enter atleast 10 characters",
+                        message: "Please enter at least 10 characters",
                       },
                       maxLength: {
                         value: 50,
@@ -149,17 +184,27 @@ const KYC = () => {
                       },
                     })}
                   />
-                  <p>{errors?.contactNumber?.message}</p>
+                  {errors?.contactNumber?.message && (
+                    <p className="text-sm text-red-500 mt-0.5">
+                      {errors.contactNumber.message}
+                    </p>
+                  )}
                 </div>
 
-                <div>
-                  <Label htmlFor="address">Address</Label>
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="address"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Address
+                  </Label>
                   <Input
                     id="address"
                     type="text"
-                    defaultValue=""
+                    placeholder="Enter address"
+                    className="h-12 border border-gray-300 mb-0"
                     {...register("address", {
-                      required: "Please enter is address!",
+                      required: "Please enter address!",
                       minLength: {
                         value: 1,
                         message: "Minimum length is required",
@@ -170,67 +215,127 @@ const KYC = () => {
                       },
                     })}
                   />
-                  <p>{errors?.address?.message}</p>
+                  {errors?.address?.message && (
+                    <p className="text-sm text-red-500 mt-0.5">
+                      {errors.address.message}
+                    </p>
+                  )}
                 </div>
 
-                <div>
-                  <Label htmlFor="image">Image</Label>
-                  <Input
-                    id="image"
-                    type="file"
-                    defaultValue=""
-                    onChange={handleUserImage}
-                    accept="image/jpeg, image/png"
-                    required
-                  />
-                  <p>{error}</p>
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="image"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Your Picture
+                  </Label>
+                  <div className="rounded-md">
+                    <Input
+                      id="image"
+                      type="file"
+                      className="h-12 border-gray-300 file:mr-4 file:mt-1 file:border-0 file:bg-gray-200 file:px-4 file:text-sm file:font-medium hover:file:bg-gray-200"
+                      onChange={handleUserImage}
+                      accept="image/jpeg, image/png"
+                      required
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <Label htmlFor="e1-image">Evidence One</Label>
-                  <Input
-                    id="e1-image"
-                    type="file"
-                    onChange={(e) => handleEvidenceImage(e, 0)}
-                    ref={e1Ref}
-                    accept="image/jpeg, image/png"
-                    required
-                  />
-                  <p>{error}</p>
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="e1-image"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Evidence One
+                  </Label>
+                  <div className="rounded-md">
+                    <Input
+                      id="e1-image"
+                      type="file"
+                      className="h-12 border-gray-300 file:mr-4 file:mt-1 file:border-0 file:bg-gray-200 file:px-4 file:text-sm file:font-medium hover:file:bg-gray-200"
+                      onChange={(e) => handleEvidenceImage(e, 0)}
+                      ref={e1Ref}
+                      accept="image/jpeg, image/png"
+                      required
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <Label htmlFor="e2-image">Evidence Two</Label>
-                  <Input
-                    id="e2-image"
-                    type="file"
-                    onChange={(e) => handleEvidenceImage(e, 1)}
-                    accept="image/jpeg, image/png"
-                    required
-                  />
-                  <p>{error}</p>
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="e2-image"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Evidence Two
+                  </Label>
+                  <div className="rounded-md">
+                    <Input
+                      id="e2-image"
+                      type="file"
+                      className="h-12 border-gray-300 file:mr-4 file:mt-1 file:border-0 file:bg-gray-200 file:px-4 file:text-sm file:font-medium hover:file:bg-gray-200"
+                      onChange={(e) => handleEvidenceImage(e, 1)}
+                      ref={e2Ref}
+                      accept="image/jpeg, image/png"
+                      required
+                    />
+                  </div>
                 </div>
 
-                <div>
-                  <Label htmlFor="description">Description</Label>
-                  <Input
+                <div className="space-y-1 px-1">
+                  <Label
+                    htmlFor="description"
+                    className="text-base font-medium pl-0.5"
+                  >
+                    Message
+                  </Label>
+                  <Textarea
                     id="description"
-                    type="text"
+                    placeholder="Enter message"
+                    className="min-h-[80px] resize-none mb-0 border-gray-300"
                     {...register("description", {
-                      required: "Please enter description",
+                      required: "Please enter message!",
                       minLength: {
                         value: 1,
                         message: "Min length is required",
                       },
                     })}
                   />
-                  <p>{errors?.description?.message}</p>
+                  {errors?.description?.message && (
+                    <p className="text-sm text-red-500 mt-0.5">
+                      {errors.description.message}
+                    </p>
+                  )}
                 </div>
+
+                {error && (
+                  <div className="rounded-md bg-red-50 p-3">
+                    <p className="text-sm text-red-500">{error}</p>
+                  </div>
+                )}
               </div>
-              <DialogFooter className="grid grid-cols-4">
+
+              <DialogFooter className="mt-6 flex gap-4 sm:justify-between px-1">
+                {/* <Button
+                type="button"
+                variant="outline"
+                className="flex-1"
+                  onClick={() => {
+                    reset();
+                    setUserImage(null);
+                    setEvidenceImages([]);
+                    setError(null);
+
+                    if (e1Ref.current) e1Ref.current.value = "";
+                    if (e2Ref.current) e2Ref.current.value = "";
+                  }}
+                >
+                  reset
+                </Button> */}
                 <DialogClose asChild>
                   <Button
-                    className="grid col-span-2"
+                    type="button"
+                    variant="outline"
+                    className="flex-1 bg-black text-white uppercase font-semibold h-10"
                     onClick={() => {
                       reset();
                       setUserImage(null);
@@ -244,7 +349,10 @@ const KYC = () => {
                     Close
                   </Button>
                 </DialogClose>
-                <Button type="submit" className="grid col-span-2">
+                <Button
+                  type="submit"
+                  className="flex-1 font-semibold uppercase h-10"
+                >
                   Submit
                 </Button>
               </DialogFooter>
