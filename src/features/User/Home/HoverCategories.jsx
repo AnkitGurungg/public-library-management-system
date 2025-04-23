d2bd3bb64c42bcb0abac346541079c939dac3f75
AnkitGurung <ankitgurung234@gmail.com>
2025-04-24 04:47:04 +0545

    design hover

--- src/features/User/Home/HoverCategories.jsx
+++ src/features/User/Home/HoverCategories.jsx
@@ -5,7 +5,7 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import useFetchDisplayCategory from "@/hooks/useFetchDisplayCategory";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronRight, ChevronUp } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -31,7 +31,7 @@ const HoverCategories = () => {
           Books {hover ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
         </Button>
       </HoverCardTrigger>
-      <HoverCardContent className="w-50 max-h-80 overflow-y-auto">
+      <HoverCardContent className="w-auto max-h-80 overflow-y-auto">
         <div className="flex flex-col gap-1">
           {displayCategory?.status === 200 &&
           Array.isArray(displayCategory?.data) &&
@@ -39,12 +39,13 @@ const HoverCategories = () => {
             displayCategory?.data?.map((element) => (
               <div
                 key={element.categoryId}
-                className="hover:bg-gray-100 px-3 w-full rounded-sm cursor-pointer"
+                className="hover:bg-gray-100 px-2 w-full rounded-sm cursor-pointer flex items-center gap-1.5"
                 onClick={() => navigate(`/books/genres/${element.categoryId}`)}
                 role="button"
                 tabIndex={0}
               >
-                <h4 className="text-base font-bold py-0.5 my-0">
+                  <ChevronRight />
+                <h4 className="text-[18px] font-bold py-0.5 my-0">
                   {element.name}
                 </h4>
               </div>
