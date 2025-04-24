d5645d0b614a9306e9df6df581feedc37c94d78b
AnkitGurung <ankitgurung234@gmail.com>
2025-04-24 05:47:12 +0545

    design popupover

--- src/features/User/Profiile/ProfilePopover.jsx
+++ src/features/User/Profiile/ProfilePopover.jsx
@@ -1,4 +1,12 @@
-import { UserRoundCheck } from "lucide-react";
+import {
+  Book,
+  ChevronRight,
+  CreditCard,
+  Heart,
+  LogOut,
+  Settings,
+  UserRoundCheck,
+} from "lucide-react";
 import {
   Popover,
   PopoverContent,
@@ -20,7 +28,7 @@ export default function ProfilePopover() {
   const logoutHandler = () => {
     localStorage.removeItem("Authorization");
     setToken("");
-    setUserInfo({});
+    setUserInfo(null);
     refetchMemberWishList();
     // localStorage.removeItem("x-refresh-token");
     navigate("/");
@@ -37,38 +45,39 @@ export default function ProfilePopover() {
         </div>
       </PopoverTrigger>
       <PopoverContent className="w-60">
-        <div className="grid gap-4">
-          <div className="space-y-2">
+        <div className="grid gap-3">
+          <div className="flex items-center gap-0.5 hover:bg-[#d9d9d9] hover:rounded-md py-1 pl-1">
+            <Heart className="mr-2 h-5 w-5" />
             <NavLink to="/member/profile/wish-list">
               <h4 className="font-medium leading-none">Wish List</h4>
             </NavLink>
           </div>
-          <div className="space-y-2">
+          <div className="flex items-center gap-0.5 hover:bg-[#d9d9d9] hover:rounded-md py-1 pl-1">
+            <Book className="mr-2 h-5 w-5" />
             <NavLink to="/member/profile/borrowed-books">
               <h4 className="font-medium leading-none">Borrowed Books</h4>
             </NavLink>
           </div>
-          <div className="space-y-2">
+          <div className="flex items-center gap-0.5 hover:bg-[#d9d9d9] hover:rounded-md py-1 pl-1">
+            <CreditCard className="mr-2 h-5 w-5" />
             <NavLink to="/member/profile/fines">
               <h4 className="font-medium leading-none">Fines</h4>
             </NavLink>
           </div>
-          <div className="grid gap-4">
+          <div className="flex items-center gap-0.5 hover:bg-[#d9d9d9] hover:rounded-md py-1 pl-1">
+            <Settings className="mr-2 h-5 w-5" />
             <div className="space-y-2 col-span-3">
               <NavLink to="/member/profile/account-settings">
                 <h4 className="font-medium leading-none">Account Settings</h4>
               </NavLink>
             </div>
-            <div className="grid grid-cols-3 items-center gap-4 hover:cursor-pointer">
-              <div className="space-y-2">
-                <h4
-                  className="font-medium leading-none"
-                  onClick={logoutHandler}
-                >
-                  Logout
-                </h4>
-              </div>
-            </div>
+          </div>
+          <div
+            className="flex items-center gap-0.5 hover:bg-[#d9d9d9] hover:rounded-md py-1 pl-1 cursor-pointer"
+            onClick={logoutHandler}
+          >
+            <LogOut className="mr-2 h-5 w-5" />
+            <h4 className="font-medium leading-none">Logout</h4>
           </div>
         </div>
       </PopoverContent>
