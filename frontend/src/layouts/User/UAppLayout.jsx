42eb0fb2f8abe725910297e637d9ca471916debc
AnkitGurung <ankitgurung234@gmail.com>
2025-07-20 19:14:18 +0545

    fix page scrolling behaviour on changing routes

--- frontend/src/layouts/User/UAppLayout.jsx
+++ frontend/src/layouts/User/UAppLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import UHeader from "./UHeader";
 import UFooter from "./UFooter";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const UAppLayout = () => {
   return (
     <div className="flex flex-col">
+      <ScrollToTop />
       <UHeader />
       <div className="flex-1 overflow-auto mt-[4.5rem]">
         <Outlet />
