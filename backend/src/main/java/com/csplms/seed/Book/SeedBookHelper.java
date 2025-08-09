b339bb60bb5c2af9d58de1c77543df9f8aebcb8f
AnkitGurung <ankitgurung234@gmail.com>
2025-08-10 01:16:26 +0545

    remove book and category seed

--- backend/src/main/java/com/csplms/seed/Book/SeedBookHelper.java
+++
@@ -1,9 +0,0 @@
-package com.csplms.seed.Book;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SeedBookHelper {
-
-
-}
--- backend/src/main/java/com/csplms/seed/Category/SeedCategoryHelper.java
+++
@@ -1,8 +0,0 @@
-package com.csplms.seed.Category;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SeedCategoryHelper {
-
-}
