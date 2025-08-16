79c05e8a45db43369fa13a5438846ff8e5c5d583
AnkitGurung <ankitgurung234@gmail.com>
2025-08-16 11:42:24 +0545

    remove web config which was supposed to allow access to pictures from browser

--- backend/src/main/java/com/csplms/config/StartupHealthLogger.java
+++ backend/src/main/java/com/csplms/config/StartupHealthLogger.java
@@ -2,41 +2,27 @@ package com.csplms.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class StartupHealthLogger implements ApplicationRunner {
 
     private final AwsProperties awsProperties;
-    private final JdbcTemplate jdbcTemplate;
-    private final S3Client s3Client;
 
     private static final Logger logger = LoggerFactory.getLogger(StartupHealthLogger.class);
 
     @Autowired
-    public StartupHealthLogger(
-            AwsProperties awsProperties,
-            JdbcTemplate jdbcTemplate,
-            S3Client s3Client
-    ) {
+    public StartupHealthLogger(AwsProperties awsProperties) {
         this.awsProperties = awsProperties;
-        this.jdbcTemplate = jdbcTemplate;
-        this.s3Client = s3Client;
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         logger.info("===== APPLICATION STARTUP INFO =====");
         logEnvironmentDetails();
-        checkDatabaseConnection();
-        checkS3Connection();
         logger.info("===== APPLICATION STARTED SUCCESSFULLY =====");
     }
 
@@ -55,52 +41,5 @@ public class StartupHealthLogger implements ApplicationRunner {
 
         // Active working dir
         logger.info("User Dir: {}", System.getProperty("user.dir"));
-    }
-
-    private void checkDatabaseConnection() {
-        int retries = 3;
-        logger.info("===== Checking Database Connection =====");
-        while (retries > 0) {
-            try {
-                jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-                logger.info("Database connection: OK ");
-                return;
-            } catch (Exception e) {
-                logger.warn("Database connection failed. Retries left: {}. Exception: {}", retries, e.getMessage());
-                sleep(3000);
-            }
-            retries--;
-        }
-        logger.error("Database connection FAILED, after retries");
-        throw new IllegalStateException("Cannot connect to database");
-    }
-
-    private void checkS3Connection() {
-        int retries = 3;
-        logger.info("===== Checking S3 Connection =====");
-        while (retries > 0) {
-            try {
-                HeadBucketRequest request = HeadBucketRequest.builder()
-                        .bucket(awsProperties.getS3BucketName())
-                        .build();
-                s3Client.headBucket(request);
-                logger.info("S3 bucket access: OK");
-                return;
-            } catch (S3Exception e) {
-                logger.warn("S3 connection failed. Retries left: {}. Exception: {}", retries, e.getMessage());
-                sleep(3000);
-            }
-            retries--;
-        }
-        logger.error("S3 bucket access FAILED, after retries");
-        throw new IllegalStateException("Cannot access S3 bucket");
-    }
-
-    private void sleep(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException ignored) {
-
-        }
     }
 }
